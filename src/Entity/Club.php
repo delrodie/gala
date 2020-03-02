c25a1eb1c16fd6f05ebdbde293a2a01a6b1dec01
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-03-02 23:04:21 +0000

    Gestion des clubs

---
+++ src/Entity/Club.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ClubRepository")
+ */
+class Club
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $nbParticipant;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNbParticipant(): ?int
+    {
+        return $this->nbParticipant;
+    }
+
+    public function setNbParticipant(?int $nbParticipant): self
+    {
+        $this->nbParticipant = $nbParticipant;
+
+        return $this;
+    }
+}
---
+++ src/Form/ClubType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Club;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClubType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class,[
+                'attr'=>['class'=>'form-control', 'placeholder'=>"club.placeholderName", 'autocomplete'=>"off"],
+                'label' => 'club.labelName'
+            ])
+            //->add('nbParticipant')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Club::class,
+        ]);
+    }
+}
